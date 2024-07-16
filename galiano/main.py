90f6f67fea66198e6b59bb8d52f60ef2e8a03064
Erik Strand <erik.strand@cba.mit.edu>
2024-07-15 23:14:03 -0700

    Extract point coordinates

--- galiano/main.py
+++ galiano/main.py
@@ -1,26 +1,16 @@
 import numpy as np
 import laspy
+import jax
+import jax.numpy as jnp
 
-with laspy.open('data/bc_092b084_3_4_2_xyes_8_utm10_2019.laz') as fh:
-    print('Points from Header:', fh.header.point_count)
-    las = fh.read()
-    print(las)
-    print('Points from data:', len(las.points))
 
-    point_format = las.point_format
-    print("point format")
-    print(point_format)
-    print(point_format.id)
-    print(list(point_format.dimension_names))
-    print("")
+def load_laz_points(laz_file):
+    with laspy.open(laz_file) as fh:
+        las = fh.read()
+        points = jnp.array([las.X, las.Y, las.Z]).T
+        print(points.shape)
+    return points
 
-    x = las.X
-    y = las.Y
-    z = las.Z
-    print(x.shape, y.shape, z.shape)
 
-    ground_pts = las.classification == 2
-    bins, counts = np.unique(las.return_number[ground_pts], return_counts=True)
-    print('Ground Point Return Number distribution:')
-    for r,c in zip(bins,counts):
-        print('    {}:{}'.format(r,c))
+if __name__ == "__main__":
+    load_laz_points("data/bc_092b084_3_4_2_xyes_8_utm10_2019.laz")
