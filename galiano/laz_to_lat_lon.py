9b25d11d44c4634835114ac8a1dcae2949435ca1
Erik Strand <erik.strand@cba.mit.edu>
2024-07-16 00:36:56 -0700

    Convert all data to lat/lon in npz format

--- galiano/laz_to_lat_lon.py
+++ galiano/laz_to_lat_lon.py
@@ -1,7 +1,7 @@
-import numpy as np
+from tqdm import tqdm
+import click
 import laspy
-import jax
-import jax.numpy as jnp
+import numpy as np
 import pyproj
 
 
@@ -9,13 +9,13 @@ def load_laz_points(laz_file):
     with laspy.open(laz_file) as fh:
         las = fh.read()
         header = las.header
-        raw_points = jnp.array([las.X, las.Y, las.Z]).T
+        raw_points = np.array([las.X, las.Y, las.Z]).T
 
         # Apply scale factors and offsets
         real_x = raw_points[:, 0] * header.x_scale + header.x_offset
         real_y = raw_points[:, 1] * header.y_scale + header.y_offset
         real_z = raw_points[:, 2] * header.z_scale + header.z_offset
-        points = jnp.vstack((real_x, real_y, real_z)).T
+        points = np.vstack((real_x, real_y, real_z)).T
 
         print("Coordinate Reference System (CRS):", header.vlrs)
         print("X Offset:", header.x_offset)
@@ -38,13 +38,7 @@ def load_laz_points(laz_file):
     return points
 
 
-if __name__ == "__main__":
-    points = load_laz_points("data/bc_092b084_3_4_2_xyes_8_utm10_2019.laz")
-    print("points", points.shape)
-    print("min", jnp.min(points, axis=0))
-    print("max", jnp.max(points, axis=0))
-
-
+def utm_to_lat_long(utm_points):
     # Define the UTM Zone 10N CRS
     utm_crs = pyproj.CRS("EPSG:3157")  # NAD83(CSRS) / UTM zone 10N
     wgs84_crs = pyproj.CRS("EPSG:4326")  # WGS84 (latitude and longitude)
@@ -52,10 +46,31 @@ if __name__ == "__main__":
     # Define a transformer to convert UTM to WGS84
     transformer = pyproj.Transformer.from_crs(utm_crs, wgs84_crs, always_xy=True)
 
-    # Example UTM coordinates (take a sample point)
-    print("eastings, northings, elevations", points[0])
-    utm_x, utm_y = points[0][:2]
+    lat_lon_points = np.zeros_like(utm_points)
+    for i in tqdm(range(utm_points.shape[0])):
+        utm_x, utm_y = utm_points[i, :2]
+        lat_lon_points[i, :2] = transformer.transform(utm_x, utm_y)
+    lat_lon_points[:, 2] = utm_points[:, 2]
+    return lat_lon_points
 
-    # Convert to latitude and longitude
-    lat, lon = transformer.transform(utm_x, utm_y)
-    print(f"Latitude: {lat}, Longitude: {lon}")
+
+@click.command()
+@click.option("--input", "-i", required=True, type=click.Path(exists=True), help="Input laz file")
+@click.option("--output", "-o", required=True, type=click.Path(), help="Output npz file")
+def main(input, output):
+    print(f"Loading {input}...")
+    utm_points = load_laz_points(input)
+    print(f"Loaded {utm_points.shape[0]} points.")
+    print("")
+
+    print("Converting UTM to Latitude and Longitude...")
+    lat_lon_points = utm_to_lat_long(utm_points)
+    print("")
+
+    print(f"Saving to {output}...")
+    np.savez(output, points=lat_lon_points)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
